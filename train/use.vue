5e4b221b6d1860d97f70c736421da4116e40d977
zhanghuai <1641893401@qq.com>
2020-06-17 20:09:07 +0800

    1

---
+++ train/use.vue
@@ -0,0 +1,24 @@
+<style>
+@import "./style/_index.sass"
+
+%border{
+  content:'';
+  position:absolute;
+  box-sizing:border-box;
+  width:100%;
+  height:100%;
+  top:0;
+  left:0;
+}
+
+%border-top {
+  &::before{
+    @extend %border;
+    @include retina-border(top)
+  }
+}
+</style>
+
+
+
+
